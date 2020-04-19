75240aa9545065aba04472e640160aa0cca74ebd
Martin Tournoij <martin@arp242.net>
2020-04-19 14:00:54 +0800

    Add osutil.Readable and osutil.Writable

---
+++ osutil/osutil_other.go
@@ -0,0 +1,15 @@
+// +build !unix
+
+package osutil
+
+import "os"
+
+// Readable reports if the file is readable by the current user.
+//
+// This is a stub for non-POSIX systems which always returns true.
+func Readable(s os.FileInfo) (bool, error) { return true, nil }
+
+// Writable reports if the file is writable by the current user.
+//
+// This is a stub for non-POSIX systems which always returns true.
+func Writable(s os.FileInfo) (bool, error) { return true, nil }
