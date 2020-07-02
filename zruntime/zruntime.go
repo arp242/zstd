46aa8f8231cabb0987054258550ec0d5f5a73027
Martin Tournoij <martin@arp242.net>
2020-07-02 22:22:52 +0800

    Add TestVerbose(), rename IsTest() to Test()

--- zruntime/zruntime.go
+++ zruntime/zruntime.go
@@ -6,10 +6,20 @@ import (
 	"strings"
 )
 
-// IsTest reports if we're running a go test command.
-func IsTest() bool {
+// Test reports if we're running a go test command.
+func Test() bool {
 	for _, a := range os.Args[1:] {
 		if strings.HasPrefix(a, "-test.") {
+			return true
+		}
+	}
+	return false
+}
+
+// TestVerbose reports if the test was started with the -v flag.
+func TestVerbose() bool {
+	for _, a := range os.Args[1:] {
+		if a == "-test.v=true" {
 			return true
 		}
 	}
