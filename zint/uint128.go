81167fffefa7544037bb1d6e88226431de371cbf
Martin Tournoij <martin@arp242.net>
2020-07-24 08:11:41 +0800

    Store as [2]uint64 instead of struct
    Doesn't really matter as such.

--- zint/uint128.go
+++ zint/uint128.go
@@ -4,13 +4,17 @@ import (
 	"database/sql/driver"
 	"fmt"
 	"strconv"
+	"strings"
 )
 
 // Uint128 is an unsigned 128-bit integer.
 //
 // Mostly intended to store UUIDs; storing it as a []byte takes up 40 bytes,
 // whereas storing it in two uint64s takes up 16 bytes.
-type Uint128 struct{ H, L uint64 }
+//
+// It's stored in so-called "big endian" format; that is, the most significant
+// bit is on the right.
+type Uint128 [2]uint64
 
 // NewUint128 creates a new uint128 from a [16]byte.
 func NewUint128(b []byte) (Uint128, error) {
@@ -18,10 +22,18 @@ func NewUint128(b []byte) (Uint128, error) {
 	return i, i.New(b)
 }
 
+func ParseUint128(s string, base int) (Uint128, error) {
+	var i Uint128
+	return i, i.Parse(s, base)
+}
+
 func (u Uint128) String() string { return u.Format(16) }
-func (i Uint128) IsZero() bool   { return i.H == 0 && i.L == 0 }
+func (i Uint128) IsZero() bool   { return i[0] == 0 && i[1] == 0 }
+
+// TODO: this is not really printin a number, but just printing the 2 numbers
+// side-by-side, rather than actually adding up the bits.
 func (u Uint128) Format(base int) string {
-	return strconv.FormatUint(u.H, base) + strconv.FormatUint(u.L, base)
+	return strconv.FormatUint(u[0], base) + "-" + strconv.FormatUint(u[1], base)
 }
 
 // New sets this uint128 from a [16]byte.
@@ -31,32 +43,51 @@ func (i *Uint128) New(b []byte) error {
 	}
 
 	_ = b[15] // bounds check hint to compiler; see golang.org/issue/14808
-	i.H = uint64(b[7]) | uint64(b[6])<<8 | uint64(b[5])<<16 | uint64(b[4])<<24 |
+	i[0] = uint64(b[7]) | uint64(b[6])<<8 | uint64(b[5])<<16 | uint64(b[4])<<24 |
 		uint64(b[3])<<32 | uint64(b[2])<<40 | uint64(b[1])<<48 | uint64(b[0])<<56
-	i.L = uint64(b[15]) | uint64(b[14])<<8 | uint64(b[13])<<16 | uint64(b[12])<<24 |
+	i[1] = uint64(b[15]) | uint64(b[14])<<8 | uint64(b[13])<<16 | uint64(b[12])<<24 |
 		uint64(b[11])<<32 | uint64(b[10])<<40 | uint64(b[9])<<48 | uint64(b[8])<<56
+	return nil
+}
+
+func (i *Uint128) Parse(str string, base int) error {
+	d := strings.Index(str, "-")
+	if d == -1 {
+		return fmt.Errorf("*Uint128.Parse: invalid format")
+	}
+
+	l, err := strconv.ParseUint(str[d+1:], base, 64)
+	if err != nil {
+		return fmt.Errorf("Uint128.Parse: L: %w", err)
+	}
+	h, err := strconv.ParseUint(str[:d], base, 64)
+	if err != nil {
+		return fmt.Errorf("Uint128.Parse: H: %w", err)
+	}
+	i[1], i[0] = l, h
 	return nil
 }
 
 func (i Uint128) Bytes() []byte {
 	b := make([]byte, 16)
 	_ = b[15] // bounds check hint to compiler; see golang.org/issue/14808
-	b[0] = byte(i.H >> 56)
-	b[1] = byte(i.H >> 48)
-	b[2] = byte(i.H >> 40)
-	b[3] = byte(i.H >> 32)
-	b[4] = byte(i.H >> 24)
-	b[5] = byte(i.H >> 16)
-	b[6] = byte(i.H >> 8)
-	b[7] = byte(i.H)
-	b[8] = byte(i.L >> 56)
-	b[9] = byte(i.L >> 48)
-	b[10] = byte(i.L >> 40)
-	b[11] = byte(i.L >> 32)
-	b[12] = byte(i.L >> 24)
-	b[13] = byte(i.L >> 16)
-	b[14] = byte(i.L >> 8)
-	b[15] = byte(i.L)
+	b[0] = byte(i[0] >> 56)
+	b[1] = byte(i[0] >> 48)
+	b[2] = byte(i[0] >> 40)
+	b[3] = byte(i[0] >> 32)
+	b[4] = byte(i[0] >> 24)
+	b[5] = byte(i[0] >> 16)
+	b[6] = byte(i[0] >> 8)
+	b[7] = byte(i[0])
+
+	b[8] = byte(i[1] >> 56)
+	b[9] = byte(i[1] >> 48)
+	b[10] = byte(i[1] >> 40)
+	b[11] = byte(i[1] >> 32)
+	b[12] = byte(i[1] >> 24)
+	b[13] = byte(i[1] >> 16)
+	b[14] = byte(i[1] >> 8)
+	b[15] = byte(i[1])
 	return b
 }
 
@@ -74,3 +105,9 @@ func (i *Uint128) Scan(v interface{}) error {
 	}
 	return i.New(b)
 }
+
+// MarshalText converts the data to a human readable representation.
+func (i Uint128) MarshalText() ([]byte, error) { return []byte(i.Format(16)), nil }
+
+// UnmarshalText parses text in to the Go data structure.
+func (i *Uint128) UnmarshalText(v []byte) error { return i.Parse(string(v), 16) }
--- zint/uint128_test.go
+++ zint/uint128_test.go
@@ -1,6 +1,7 @@
 package zint
 
 import (
+	"fmt"
 	"testing"
 
 	"zgo.at/zstd/ztest"
@@ -8,36 +9,103 @@ import (
 
 func TestUint128(t *testing.T) {
 	ztest.MustInline(t, "zgo.at/zstd/zint",
-		"NewUint128",
-		"Uint128.IsZero",
-		"Uint128.String",
-		// Too complex:
-		// "Uint128.Bytes",
-		// "(*Uint128).New",
-	)
+		"NewUint128", "Uint128.IsZero", "Uint128.String")
 
-	n := []byte{44, 25, 67, 129, 231, 4, 77, 72, 157, 135, 42, 180, 126, 162, 176, 131}
-
-	i, err := NewUint128(n)
-	if err != nil {
-		t.Fatal(err)
+	tests := []struct {
+		in               []byte
+		wantB10, wantB16 string
+		wantZero         bool
+	}{
+		{
+			[]byte{44, 25, 67, 129, 231, 4, 77, 72, 157, 135, 42, 180, 126, 162, 176, 131},
+			"3177645237292256584-11351088340517695619", "2c194381e7044d48-9d872ab47ea2b083", false,
+		},
+		{
+			[]byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+			"0-0", "0-0", true,
+		},
+		{
+			[]byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+			"0-1", "0-1", false,
+		},
+		{
+			[]byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+			"0-256", "0-100", false,
+		},
+		{
+			[]byte{0, 0, 0, 0, 0, 0, 0, 0, 255, 255, 255, 255, 255, 255, 255, 255},
+			"0-18446744073709551615", "0-ffffffffffffffff", false,
+		},
+		{
+			[]byte{0, 0, 0, 0, 0, 0, 0, 1, 255, 255, 255, 255, 255, 255, 255, 255},
+			"1-18446744073709551615", "1-ffffffffffffffff", false,
+		},
+		{
+			[]byte{255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255},
+			"18446744073709551615-18446744073709551615",
+			"ffffffffffffffff-ffffffffffffffff",
+			false,
+		},
+		{
+			[]byte{0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+			"1-0", "1-0", false,
+		},
 	}
 
-	b := i.Bytes()
-	if string(n) != string(b) {
-		t.Errorf("Bytes()\nwant: %v\ngot:  %v", n, b)
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%v", tt.in), func(t *testing.T) {
+			i, err := NewUint128(tt.in)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if !tt.wantZero && i.IsZero() {
+				t.Fatal("IsZero()")
+			}
+
+			b := i.Bytes()
+			if string(tt.in) != string(b) {
+				t.Errorf("Bytes()\nwant: %v\ngot:  %v", tt.in, b)
+			}
+
+			if i.Format(10) != tt.wantB10 {
+				t.Errorf("Format(10)\nwant: %s\ngot:  %s", tt.wantB10, i.Format(10))
+			}
+			if i.String() != tt.wantB16 {
+				t.Errorf("String()\nwant: %s\ngot:  %s", tt.wantB16, i.String())
+			}
+
+			p10, err := ParseUint128(i.Format(10), 10)
+			if err != nil {
+				t.Error(err)
+			} else if p10.Format(10) != tt.wantB10 {
+				t.Errorf("Parse(10)\nwant: %s\ngot:  %s", tt.wantB10, p10.Format(10))
+			}
+
+			p16, err := ParseUint128(i.Format(16), 16)
+			if err != nil {
+				t.Error(err)
+			} else if p16.Format(16) != tt.wantB16 {
+				t.Errorf("Parse(16)\nwant: %s\ngot:  %s", tt.wantB16, p16.Format(16))
+			}
+		})
 	}
 
-	_, err = NewUint128(nil)
-	if err == nil {
-		t.Fatal(err)
-	}
+	t.Run("invalid", func(t *testing.T) {
+		for _, invalid := range [][]byte{nil, {1}, {1}} {
+			i, err := NewUint128(invalid)
+			if err == nil {
+				t.Fatal(err)
+			}
+			if i[0] != 0 || i[1] != 0 {
+				t.Fatal(err)
+			}
+		}
+	})
 
-	if i.IsZero() {
-		t.Fatal()
-	}
-	i2 := Uint128{}
-	if !i2.IsZero() {
-		t.Fatal()
-	}
+	t.Run("zero", func(t *testing.T) {
+		i2 := Uint128{}
+		if !i2.IsZero() {
+			t.Fatal()
+		}
+	})
 }
