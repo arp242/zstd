350d4ae0eb40be22e45bf71bba5edd4cdb7f1091
Martin Tournoij <martin@arp242.net>
2020-07-25 16:15:11 +0800

    Some date helpers

--- ztime/ztime.go
+++ ztime/ztime.go
@@ -28,3 +28,31 @@ func FormatDuration(d time.Duration) string {
 
 	return d.Round(time.Second).String()
 }
+
+// LeapYear reports if this year is a leap year according to the Gregorian
+// calendar.
+func LeapYear(t time.Time) bool {
+	y := t.Year()
+	return y%4 == 0 && (y%100 != 0 || y%400 == 0)
+}
+
+// DaysInMonth gets the number of days for the month.
+func DaysInMonth(t time.Time) int {
+	m := t.Month()
+	if m == 2 && LeapYear(t) {
+		return 29
+	}
+	switch m {
+	case 1, 3, 5, 7, 8, 10, 12:
+		return 31
+	default:
+		return 30
+	}
+}
+
+// LastInMonth reports if the current day is the last day in this month.
+func LastInMonth(t time.Time) bool { return t.Day() == DaysInMonth(t) }
+
+func StartOfDay(t time.Time) time.Time {
+	return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location())
+}
