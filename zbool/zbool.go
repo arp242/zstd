4b5d5ef87ea2a3d94297eaa2df2e6f8b1c442bf9
Martin Tournoij <martin@arp242.net>
2021-02-25 08:56:19 +0800

    Add zbool

---
+++ zbool/zbool.go
@@ -0,0 +1,134 @@
+package zbool
+
+import (
+	"database/sql/driver"
+	"fmt"
+	"strings"
+)
+
+// Bool converts various types to a boolean.
+//
+// It's always stored as an integer in the database (the only cross-platform way
+// in SQL).
+//
+// Supported types:
+//
+//   bool
+//   int* and float*     0 or 1
+//   []byte and string   "1", "true", "on", "0", "false", "off"
+//   nil                 defaults to false
+type Bool bool
+
+// Scan converts the data from the DB.
+func (b *Bool) Scan(src interface{}) error {
+	if b == nil {
+		return fmt.Errorf("zdb.Bool: not initialized")
+	}
+
+	switch v := src.(type) {
+	default:
+		return fmt.Errorf("zdb.Bool: unsupported type %T", src)
+	case nil:
+		*b = false
+	case bool:
+		*b = Bool(v)
+	case int:
+		*b = v != 0
+	case int8:
+		*b = v != 0
+	case int16:
+		*b = v != 0
+	case int32:
+		*b = v != 0
+	case int64:
+		*b = v != 0
+	case uint:
+		*b = v != 0
+	case uint8:
+		*b = v != 0
+	case uint16:
+		*b = v != 0
+	case uint32:
+		*b = v != 0
+	case uint64:
+		*b = v != 0
+	case float32:
+		*b = v != 0
+	case float64:
+		*b = v != 0
+
+	case []byte, string:
+		var text string
+		raw, ok := v.([]byte)
+		if !ok {
+			text = v.(string)
+		} else if len(raw) == 1 {
+			// Handle the bit(1) column type.
+			*b = raw[0] == 1
+			return nil
+		} else {
+			text = string(raw)
+		}
+
+		switch strings.TrimSpace(strings.ToLower(text)) {
+		case "true", "1", "on":
+			*b = true
+		case "false", "0", "off":
+			*b = false
+		default:
+			return fmt.Errorf("zdb.Bool: invalid value %q", text)
+		}
+	}
+
+	return nil
+}
+
+// Value converts a bool type into a number to persist it in the database.
+func (b Bool) Value() (driver.Value, error) {
+	if b {
+		return int64(1), nil
+	}
+	return int64(0), nil
+}
+
+// MarshalJSON converts the data to JSON.
+func (b Bool) MarshalJSON() ([]byte, error) {
+	return []byte(fmt.Sprintf("%t", b)), nil
+}
+
+// UnmarshalJSON converts the data from JSON.
+func (b *Bool) UnmarshalJSON(text []byte) error {
+	switch string(text) {
+	case "true":
+		*b = true
+		return nil
+	case "false":
+		*b = false
+		return nil
+	default:
+		return fmt.Errorf("zdb.Bool: unknown value: %s", text)
+	}
+}
+
+// MarshalText converts the data to a human readable representation.
+func (b Bool) MarshalText() ([]byte, error) {
+	return []byte(fmt.Sprintf("%t", b)), nil
+}
+
+// UnmarshalText parses text in to the Go data structure.
+func (b *Bool) UnmarshalText(text []byte) error {
+	if b == nil {
+		return fmt.Errorf("zdb.Bool: not initialized")
+	}
+
+	switch strings.TrimSpace(strings.ToLower(string(text))) {
+	case "true", "1", `"true"`:
+		*b = true
+	case "false", "0", `"false"`:
+		*b = false
+	default:
+		return fmt.Errorf("zdb.Bool: invalid value %q", text)
+	}
+
+	return nil
+}
---
+++ zbool/zbool_test.go
@@ -0,0 +1,128 @@
+package zbool
+
+import (
+	"database/sql/driver"
+	"fmt"
+	"reflect"
+	"testing"
+	"time"
+
+	"zgo.at/zstd/ztest"
+)
+
+func TestBool(t *testing.T) {
+	t.Run("value", func(t *testing.T) {
+		cases := []struct {
+			in   Bool
+			want driver.Value
+		}{
+			{false, int64(0)},
+			{true, int64(1)},
+		}
+
+		for _, tc := range cases {
+			t.Run(fmt.Sprintf("%v", tc.in), func(t *testing.T) {
+				out, err := tc.in.Value()
+				if err != nil {
+					t.Fatal(err)
+				}
+				if out != tc.want {
+					t.Errorf("\nout:  %#v\nwant: %#v\n", out, tc.want)
+				}
+			})
+		}
+	})
+
+	t.Run("scan", func(t *testing.T) {
+		cases := []struct {
+			in      interface{}
+			want    Bool
+			wantErr string
+		}{
+			{[]byte("true"), true, ""},
+			{float64(1.0), true, ""},
+			{[]byte{1}, true, ""},
+			{int64(1), true, ""},
+			{"true", true, ""},
+			{true, true, ""},
+			{"1", true, ""},
+
+			{[]byte("false"), false, ""},
+			{float64(0), false, ""},
+			{[]byte{0}, false, ""},
+			{int64(0), false, ""},
+			{"false", false, ""},
+			{false, false, ""},
+			{"0", false, ""},
+			{nil, false, ""},
+
+			{"not a valid bool", false, "invalid value \"not a valid bool\""},
+			{time.Time{}, false, "unsupported type time.Time"},
+		}
+
+		for _, tc := range cases {
+			t.Run(fmt.Sprintf("%v", tc.in), func(t *testing.T) {
+				var out Bool
+				err := out.Scan(tc.in)
+				if !ztest.ErrorContains(err, tc.wantErr) {
+					t.Errorf("\nout:  %#v\nwant: %#v\n", err, tc.wantErr)
+				}
+				if !reflect.DeepEqual(out, tc.want) {
+					t.Errorf("\nout:  %#v\nwant: %#v\n", out, tc.want)
+				}
+			})
+		}
+	})
+
+	t.Run("marshalText", func(t *testing.T) {
+		cases := []struct {
+			in      Bool
+			want    []byte
+			wantErr string
+		}{
+			{false, []byte("false"), ""},
+			{true, []byte("true"), ""},
+		}
+
+		for _, tc := range cases {
+			t.Run(fmt.Sprintf("%v", tc.in), func(t *testing.T) {
+				out, err := tc.in.MarshalText()
+				if !ztest.ErrorContains(err, tc.wantErr) {
+					t.Errorf("\nout:  %#v\nwant: %#v\n", err, tc.wantErr)
+				}
+				if !reflect.DeepEqual(out, tc.want) {
+					t.Errorf("\nout:  %#v\nwant: %#v\n", out, tc.want)
+				}
+			})
+		}
+	})
+
+	t.Run("unmarshalText", func(t *testing.T) {
+		cases := []struct {
+			in      []byte
+			want    Bool
+			wantErr string
+		}{
+			{[]byte("  true  "), true, ""},
+			{[]byte(` "true"`), true, ""},
+			{[]byte(`  1 `), true, ""},
+			{[]byte("false  "), false, ""},
+			{[]byte(`"false" `), false, ""},
+			{[]byte(` 0 `), false, ""},
+			{[]byte(`not a valid bool`), false, "invalid value \"not a valid bool\""},
+		}
+
+		for _, tc := range cases {
+			t.Run(fmt.Sprintf("%v", tc.in), func(t *testing.T) {
+				var out Bool
+				err := out.UnmarshalText(tc.in)
+				if !ztest.ErrorContains(err, tc.wantErr) {
+					t.Errorf("\nout:  %#v\nwant: %#v\n", err, tc.wantErr)
+				}
+				if !reflect.DeepEqual(out, tc.want) {
+					t.Errorf("\nout:  %#v\nwant: %#v\n", out, tc.want)
+				}
+			})
+		}
+	})
+}
