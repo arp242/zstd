f5ab74ffd3f5d4efece6305cc14b39a4e07147e0
Martin Tournoij <martin@arp242.net>
2021-03-08 16:58:13 +0800

    Add ztime.Takes() and ztime.DurationAs()

--- ztime/ztime.go
+++ ztime/ztime.go
@@ -3,6 +3,8 @@ package ztime
 
 import (
 	"fmt"
+	"math"
+	"strings"
 	"time"
 )
 
@@ -10,6 +12,38 @@ var (
 	day    = 24 * time.Hour
 	day100 = 24 * time.Hour * 100
 )
+
+// Takes records how long the execution of f takes.
+func Takes(f func()) time.Duration {
+	s := time.Now()
+	f()
+	return time.Now().Sub(s)
+}
+
+// DurationAs formats a duration as the given time unit.
+//
+// Use Round() if you want to limit the precision; for example:
+//
+//   DurationAs(d.Round(time.Microsecond), time.Millisecond)
+func DurationAs(d, as time.Duration) string {
+	f := float64(d) / float64(as)
+	_, frac := math.Modf(f)
+	if frac == 0 {
+		return fmt.Sprintf("%.0f", f)
+	}
+
+	s := strings.TrimRight(fmt.Sprintf("%f", f), "0")
+	if s == "0." {
+		s = fmt.Sprintf("%.999f", f)
+		for i, c := range s {
+			if c != '0' && c != '.' {
+				s = s[:i+1]
+				break
+			}
+		}
+	}
+	return s
+}
 
 // FormatDuration formats duration as a concise string. It's less accurate than
 // Duration.String(), but shorter.
--- ztime/ztime_test.go
+++ ztime/ztime_test.go
@@ -5,6 +5,35 @@ import (
 	"time"
 )
 
+func TestTakes(t *testing.T) {
+	tt := Takes(func() { time.Sleep(50 * time.Millisecond) })
+	if tt < 50*time.Millisecond || tt > 52*time.Millisecond {
+		t.Error(tt)
+	}
+}
+
+func TestDurationAs(t *testing.T) {
+	tests := []struct {
+		d, as time.Duration
+		want  string
+	}{
+		{50 * time.Millisecond, time.Microsecond, "50000"},
+		{50 * time.Microsecond, time.Millisecond, "0.05"},
+		{1, time.Hour, "0.0000000000002"},
+		{1261616533, time.Millisecond, "1261.616533"},
+		{time.Duration(1261616533).Round(time.Microsecond), time.Millisecond, "1261.617"},
+	}
+
+	for _, tt := range tests {
+		t.Run("", func(t *testing.T) {
+			got := DurationAs(tt.d, tt.as)
+			if got != tt.want {
+				t.Errorf("\ngot:  %q\nwant: %q", got, tt.want)
+			}
+		})
+	}
+}
+
 func TestFormatDuration(t *testing.T) {
 	tests := []struct {
 		in   time.Duration
