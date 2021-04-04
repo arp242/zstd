669c38f82e32e47d99641da86e45027032515b3f
Martin Tournoij <martin@arp242.net>
2021-04-05 07:29:03 +0800

    Add ztime.StartOf(), EndOf(), Add()

--- ztime/ztime.go
+++ ztime/ztime.go
@@ -9,10 +9,194 @@ import (
 	"time"
 )
 
-var (
-	day    = 24 * time.Hour
-	day100 = 24 * time.Hour * 100
+// Time wraps time.Time to add {Start,End}Of and {For,Back}ward. To make it a
+// bit more convenient to run several operations.
+type Time struct{ time.Time }
+
+func (t Time) StartOf(p Period) Time    { return Time{StartOf(t.Time, p)} }
+func (t Time) EndOf(p Period) Time      { return Time{EndOf(t.Time, p)} }
+func (t Time) Add(n int, p Period) Time { return Time{Add(t.Time, n, p)} }
+
+func (t Time) AddTime(d time.Duration) Time         { return Time{t.Time.Add(d)} }
+func (t Time) AddDate(years, months, days int) Time { return Time{t.Time.AddDate(years, months, days)} }
+func (t Time) In(loc *time.Location) Time           { return Time{t.Time.In(loc)} }
+func (t Time) Local() Time                          { return Time{t.Time.Local()} }
+func (t Time) Round(d time.Duration) Time           { return Time{t.Time.Round(d)} }
+func (t Time) Truncate(d time.Duration) Time        { return Time{t.Time.Truncate(d)} }
+func (t Time) UTC() Time                            { return Time{t.Time.UTC()} }
+
+// Period to adjust a time by.
+type Period uint8
+
+// Periods to adjust a time by.
+const (
+	_ Period = iota + 1
+	Second
+	Minute
+	QuarterHour
+	HalfHour
+	Hour
+	Day
+	WeekMonday
+	WeekSunday
+	Month
+	Quarter
+	HalfYear
+	Year
 )
+
+func (p Period) String() string {
+	switch p {
+	case Second:
+		return "second"
+	case Minute:
+		return "minute"
+	case QuarterHour:
+		return "quarter hour"
+	case HalfHour:
+		return "half hour"
+	case Hour:
+		return "hour"
+	case Day:
+		return "day"
+	case WeekMonday:
+		return "week (Monday)"
+	case WeekSunday:
+		return "week (Sunday)"
+	case Month:
+		return "month"
+	case Quarter:
+		return "quarter"
+	case HalfYear:
+		return "half year"
+	case Year:
+		return "year"
+	default:
+		return ""
+	}
+}
+
+// StartOf adjusts the time to the start of the given period.
+func StartOf(t time.Time, p Period) time.Time {
+	y, m, d, h, min, s, ns, l := t.Year(), int(t.Month()), t.Day(), t.Hour(), t.Minute(), t.Second(), t.Nanosecond(), t.Location()
+	ns = 0
+	switch p {
+	case Second:
+		// ns is already set.
+	case Minute:
+		s = 0
+	case QuarterHour:
+		min, s = min-min%15, 0
+	case HalfHour:
+		min, s = min-min%30, 0
+	case Hour:
+		min, s = 0, 0
+	case Day:
+		h, min, s = 0, 0, 0
+	case WeekMonday:
+		wd := int(t.Weekday()) - 1
+		if wd == -1 {
+			wd = 6
+		}
+		d, h, min, s = d-wd, 0, 0, 0
+	case WeekSunday:
+		d, h, min, s = d-int(t.Weekday()), 0, 0, 0
+	case Month:
+		d, h, min, s = 1, 0, 0, 0
+	case Quarter:
+		m, d, h, min, s = m-(m-1)%3, 1, 0, 0, 0
+	case HalfYear:
+		m, d, h, min, s = m-(m-1)%6, 1, 0, 0, 0
+	case Year:
+		m, d, h, min, s = 1, 1, 0, 0, 0
+	default:
+		panic(fmt.Sprintf("ztime.StartOf: invalid Period value: %v", p))
+	}
+	return time.Date(y, time.Month(m), d, h, min, s, ns, l)
+}
+
+// EndOf adjusts the time to the end of the given period.
+func EndOf(t time.Time, p Period) time.Time {
+	y, m, d, h, min, s, ns, l := t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), t.Second(), t.Nanosecond(), t.Location()
+	ns = 999999999
+	switch p {
+	case Second:
+		// ns is already set.
+	case Minute:
+		s = 59
+	case QuarterHour:
+		min, s = StartOf(t, p).Add(14*time.Minute).Minute(), 59
+	case HalfHour:
+		min, s = StartOf(t, p).Add(29*time.Minute).Minute(), 59
+	case Hour:
+		min, s = 59, 59
+	case Day:
+		h, min, s = 23, 59, 59
+	case WeekMonday, WeekSunday:
+		t = StartOf(t, p).AddDate(0, 0, 7)
+		m, d, h, min, s = t.Month(), t.Day(), 23, 59, 59
+	case Month:
+		d, h, min, s = DaysInMonth(t), 23, 59, 59
+	case Quarter:
+		t = StartOf(t, p).AddDate(0, 3, 0).Add(-1)
+		m, d, h, min, s = t.Month(), DaysInMonth(t), 23, 59, 59
+	case HalfYear:
+		t = StartOf(t, p).AddDate(0, 6, 0).Add(-1)
+		m, d, h, min, s = t.Month(), DaysInMonth(t), 23, 59, 59
+	case Year:
+		m, d, h, min, s = 12, 31, 23, 59, 59
+	default:
+		panic(fmt.Sprintf("ztime.EndOf: invalid Period value: %v", p))
+	}
+	return time.Date(y, m, d, h, min, s, ns, l)
+}
+
+// Add a time period.
+//
+// For Month, Quarter, and HalfYear, and Year the time will be set to the last
+// day of the month if the new month has fewer days than the current day.
+func Add(t time.Time, n int, p Period) time.Time {
+	y, m, d, h, min, s, ns, l := t.Year(), int(t.Month()), t.Day(), t.Hour(), t.Minute(), t.Second(), t.Nanosecond(), t.Location()
+	switch p {
+	case Second:
+		s += n
+	case Minute:
+		m += n
+	case QuarterHour:
+		min += 15 * n
+	case HalfHour:
+		min += 30 * n
+	case Hour:
+		h += n
+	case Day:
+		h += 24 * n
+	case WeekMonday, WeekSunday:
+		d += 7 * n
+	case Month:
+		m += n
+		if n := DaysInMonth(time.Date(y, time.Month(m), 1, h, min, s, ns, l)); DaysInMonth(t) > n {
+			d = n
+		}
+	case Quarter:
+		m += 3 * n
+		if n := DaysInMonth(time.Date(y, time.Month(m), 1, h, min, s, ns, l)); DaysInMonth(t) > n {
+			d = n
+		}
+	case HalfYear:
+		m += 6 * n
+		if n := DaysInMonth(time.Date(y, time.Month(m), 1, h, min, s, ns, l)); DaysInMonth(t) > n {
+			d = n
+		}
+	case Year:
+		y += n
+		if n := DaysInMonth(time.Date(y, time.Month(m), 1, h, min, s, ns, l)); DaysInMonth(t) > n {
+			d = n // To deal with leap years
+		}
+	default:
+		panic(fmt.Sprintf("ztime.Next: invalid Period value: %v", p))
+	}
+	return time.Date(y, time.Month(m), d, h, min, s, ns, l)
+}
 
 // Takes records how long the execution of f takes.
 func Takes(f func()) time.Duration {
@@ -56,24 +240,6 @@ func DurationAs(d, as time.Duration) string {
 	return s
 }
 
-// FormatDuration formats duration as a concise string. It's less accurate than
-// Duration.String(), but shorter.
-func FormatDuration(d time.Duration) string {
-	switch {
-	case d >= day100:
-		h := int(d.Round(time.Hour).Hours())
-		return fmt.Sprintf("%dd", h/24)
-	case d > day:
-		h := int(d.Round(time.Hour).Hours())
-		return fmt.Sprintf("%dd%dh", h/24, h%24)
-	case d >= 10*time.Minute:
-		s := d.Round(time.Minute).String()
-		return s[:len(s)-2]
-	}
-
-	return d.Round(time.Second).String()
-}
-
 // LeapYear reports if this year is a leap year according to the Gregorian
 // calendar.
 func LeapYear(t time.Time) bool {
@@ -83,11 +249,13 @@ func LeapYear(t time.Time) bool {
 
 // DaysInMonth gets the number of days for the month.
 func DaysInMonth(t time.Time) int {
-	m := t.Month()
-	if m == 2 && LeapYear(t) {
-		return 29
+	if t.Month() == 2 {
+		if LeapYear(t) {
+			return 29
+		}
+		return 28
 	}
-	switch m {
+	switch t.Month() {
 	case 1, 3, 5, 7, 8, 10, 12:
 		return 31
 	default:
@@ -96,8 +264,6 @@ func DaysInMonth(t time.Time) int {
 }
 
 // LastInMonth reports if the current day is the last day in this month.
-func LastInMonth(t time.Time) bool { return t.Day() == DaysInMonth(t) }
-
-func StartOfDay(t time.Time) time.Time {
-	return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location())
+func LastInMonth(t time.Time) bool {
+	return t.Day() == DaysInMonth(t)
 }
--- ztime/ztime_test.go
+++ ztime/ztime_test.go
@@ -1,8 +1,12 @@
 package ztime
 
 import (
+	"fmt"
+	"strings"
 	"testing"
 	"time"
+
+	"zgo.at/zstd/ztest"
 )
 
 func TestTakes(t *testing.T) {
@@ -34,28 +38,155 @@ func TestDurationAs(t *testing.T) {
 	}
 }
 
-func TestFormatDuration(t *testing.T) {
-	tests := []struct {
-		in   time.Duration
-		want string
-	}{
-		{10 * time.Second, "10s"},
-		{130 * time.Second, "2m10s"},
-		{1606 * time.Second, "27m"},
-		{3664 * time.Second, "1h1m"},
-		{(86400 + 3664) * time.Second, "1d1h"},
-		{(86400*15 + 3664) * time.Second, "15d1h"},
-		{(86400*17 + 3664*10) * time.Second, "17d10h"},
-		{(86400 * 100) * time.Second, "100d"},
-		{(86400*204 + 3664*2) * time.Second, "204d"},
+func TestStartOf(t *testing.T) {
+	var (
+		periods = []Period{Second, Minute, QuarterHour, HalfHour, Hour, Day, WeekMonday, WeekSunday, Month, Quarter, HalfYear, Year}
+		f       = "2006-01-02 15:04:05.999999999"
+		tt      = Time{time.Date(2020, 6, 18, 14, 49, 20, 666, time.UTC)}
+		h       = new(strings.Builder)
+	)
+	h.WriteString("       StartOf: " + tt.Format(f) + "\n")
+	for _, p := range periods {
+		pad := strings.Repeat(" ", 14-len(p.String()))
+		fmt.Fprintln(h, p.String(), pad,
+			tt.StartOf(p).Format(f),
+			"", tt.StartOf(p).AddTime(-1).StartOf(p).Format(f),
+			"", tt.StartOf(p).AddTime(-1).StartOf(p).AddTime(-1).StartOf(p).Format(f),
+			"", tt.StartOf(p).AddTime(-1).StartOf(p).AddTime(-1).StartOf(p).AddTime(-1).StartOf(p).Format(f))
 	}
 
-	for _, tt := range tests {
-		t.Run("", func(t *testing.T) {
-			out := FormatDuration(tt.in)
-			if out != tt.want {
-				t.Errorf("\nout:  %q\nwant: %q", out, tt.want)
-			}
-		})
+	have := h.String()
+	want := `
+		       StartOf: 2020-06-18 14:49:20.000000666
+		second          2020-06-18 14:49:20  2020-06-18 14:49:19  2020-06-18 14:49:18  2020-06-18 14:49:17
+		minute          2020-06-18 14:49:00  2020-06-18 14:48:00  2020-06-18 14:47:00  2020-06-18 14:46:00
+		quarter hour    2020-06-18 14:45:00  2020-06-18 14:30:00  2020-06-18 14:15:00  2020-06-18 14:00:00
+		half hour       2020-06-18 14:30:00  2020-06-18 14:00:00  2020-06-18 13:30:00  2020-06-18 13:00:00
+		hour            2020-06-18 14:00:00  2020-06-18 13:00:00  2020-06-18 12:00:00  2020-06-18 11:00:00
+		day             2020-06-18 00:00:00  2020-06-17 00:00:00  2020-06-16 00:00:00  2020-06-15 00:00:00
+		week (Monday)   2020-06-15 00:00:00  2020-06-08 00:00:00  2020-06-01 00:00:00  2020-05-25 00:00:00
+		week (Sunday)   2020-06-14 00:00:00  2020-06-07 00:00:00  2020-05-31 00:00:00  2020-05-24 00:00:00
+		month           2020-06-01 00:00:00  2020-05-01 00:00:00  2020-04-01 00:00:00  2020-03-01 00:00:00
+		quarter         2020-04-01 00:00:00  2020-01-01 00:00:00  2019-10-01 00:00:00  2019-07-01 00:00:00
+		half year       2020-01-01 00:00:00  2019-07-01 00:00:00  2019-01-01 00:00:00  2018-07-01 00:00:00
+		year            2020-01-01 00:00:00  2019-01-01 00:00:00  2018-01-01 00:00:00  2017-01-01 00:00:00`
+	if d := ztest.Diff(have, want, ztest.DiffNormalizeWhitespace); d != "" {
+		t.Error(d)
+	}
+}
+
+func TestEndOf(t *testing.T) {
+	var (
+		periods = []Period{Second, Minute, QuarterHour, HalfHour, Hour, Day, WeekMonday, WeekSunday, Month, Quarter, HalfYear, Year}
+		f       = "2006-01-02 15:04:05.999999999"
+		tt      = Time{time.Date(2020, 6, 18, 14, 49, 20, 666, time.UTC)}
+		h       = new(strings.Builder)
+	)
+	h.WriteString("\n         EndOf: " + tt.Format(f) + "\n")
+	for _, p := range periods {
+		pad := strings.Repeat(" ", 14-len(p.String()))
+		fmt.Fprintln(h, p.String(), pad,
+			tt.EndOf(p).Format(f),
+			"", tt.EndOf(p).AddTime(1).EndOf(p).Format(f),
+			"", tt.EndOf(p).AddTime(1).EndOf(p).AddTime(1).EndOf(p).Format(f),
+			"", tt.EndOf(p).AddTime(1).EndOf(p).AddTime(1).EndOf(p).AddTime(1).EndOf(p).Format(f))
+	}
+
+	have := h.String()
+	want := `
+		         EndOf: 2020-06-18 14:49:20.000000666
+		second          2020-06-18 14:49:20.999999999  2020-06-18 14:49:21.999999999  2020-06-18 14:49:22.999999999  2020-06-18 14:49:23.999999999
+		minute          2020-06-18 14:49:59.999999999  2020-06-18 14:50:59.999999999  2020-06-18 14:51:59.999999999  2020-06-18 14:52:59.999999999
+		quarter hour    2020-06-18 14:59:59.999999999  2020-06-18 15:14:59.999999999  2020-06-18 15:29:59.999999999  2020-06-18 15:44:59.999999999
+		half hour       2020-06-18 14:59:59.999999999  2020-06-18 15:29:59.999999999  2020-06-18 15:59:59.999999999  2020-06-18 16:29:59.999999999
+		hour            2020-06-18 14:59:59.999999999  2020-06-18 15:59:59.999999999  2020-06-18 16:59:59.999999999  2020-06-18 17:59:59.999999999
+		day             2020-06-18 23:59:59.999999999  2020-06-19 23:59:59.999999999  2020-06-20 23:59:59.999999999  2020-06-21 23:59:59.999999999
+		week (Monday)   2020-06-22 23:59:59.999999999  2020-06-29 23:59:59.999999999  2020-07-06 23:59:59.999999999  2020-07-13 23:59:59.999999999
+		week (Sunday)   2020-06-21 23:59:59.999999999  2020-06-28 23:59:59.999999999  2020-07-05 23:59:59.999999999  2020-07-12 23:59:59.999999999
+		month           2020-06-30 23:59:59.999999999  2020-07-31 23:59:59.999999999  2020-08-31 23:59:59.999999999  2020-09-30 23:59:59.999999999
+		quarter         2020-06-30 23:59:59.999999999  2020-09-30 23:59:59.999999999  2020-12-31 23:59:59.999999999  2021-03-31 23:59:59.999999999
+		half year       2020-06-30 23:59:59.999999999  2020-12-31 23:59:59.999999999  2021-06-30 23:59:59.999999999  2021-12-31 23:59:59.999999999
+		year            2020-12-31 23:59:59.999999999  2021-12-31 23:59:59.999999999  2022-12-31 23:59:59.999999999  2023-12-31 23:59:59.999999999`
+	if d := ztest.Diff(have, want, ztest.DiffNormalizeWhitespace); d != "" {
+		t.Error(d)
+	}
+}
+
+func TestStartOfWeek(t *testing.T) {
+	var (
+		mon = Time{time.Date(2021, 4, 5, 14, 49, 20, 666, time.UTC)}
+		sun = Time{time.Date(2021, 4, 4, 14, 49, 20, 666, time.UTC)}
+		f   = "Mon Jan _2"
+		h   = new(strings.Builder)
+	)
+
+	h.WriteString("Monday:\n")
+	for i := 0; i < 7; i++ {
+		fmt.Fprintf(h, "%d  %s → %s\n", i, mon.Add(i, Day).Format(f),
+			mon.Add(i, Day).StartOf(WeekMonday).Format(f))
+	}
+	h.WriteString("\nSunday:\n")
+	for i := 0; i < 7; i++ {
+		fmt.Fprintf(h, "%d  %s → %s\n", i, sun.Add(i, Day).Format(f),
+			sun.Add(i, Day).StartOf(WeekSunday).Format(f))
+	}
+
+	have := h.String()
+	want := `
+		Monday:
+		0  Mon Apr  5 → Mon Apr  5
+		1  Tue Apr  6 → Mon Apr  5
+		2  Wed Apr  7 → Mon Apr  5
+		3  Thu Apr  8 → Mon Apr  5
+		4  Fri Apr  9 → Mon Apr  5
+		5  Sat Apr 10 → Mon Apr  5
+		6  Sun Apr 11 → Mon Apr  5
+
+		Sunday:
+		0  Sun Apr  4 → Sun Apr  4
+		1  Mon Apr  5 → Sun Apr  4
+		2  Tue Apr  6 → Sun Apr  4
+		3  Wed Apr  7 → Sun Apr  4
+		4  Thu Apr  8 → Sun Apr  4
+		5  Fri Apr  9 → Sun Apr  4
+		6  Sat Apr 10 → Sun Apr  4`
+	if d := ztest.Diff(have, want, ztest.DiffNormalizeWhitespace); d != "" {
+		t.Error(d)
+	}
+}
+
+func TestAdd(t *testing.T) {
+	var (
+		f = "2006-01-02 15:04:05"
+		h = new(strings.Builder)
+	)
+	test := func(p Period, tt time.Time) {
+		pad := strings.Repeat(" ", 10-len(p.String()))
+		fmt.Fprintln(h, p.String(), tt.Format(f), pad,
+			"", Add(tt, 1, p).Format(f),
+			"", Add(tt, 2, p).Format(f),
+			"", Add(tt, 3, p).Format(f),
+			"\n"+strings.Repeat(" ", 32),
+			Add(tt, -1, p).Format(f),
+			"", Add(tt, -2, p).Format(f),
+			"", Add(tt, -3, p).Format(f))
+	}
+	test(Month, time.Date(2020, 1, 31, 14, 49, 20, 666, time.UTC))
+	test(Quarter, time.Date(2020, 1, 31, 14, 49, 20, 666, time.UTC))
+	test(HalfYear, time.Date(2020, 3, 31, 14, 49, 20, 666, time.UTC))
+	test(Year, time.Date(2020, 2, 29, 14, 49, 20, 666, time.UTC))
+
+	have := h.String()
+	want := `
+		month 2020-01-31 14:49:20        2020-02-29 14:49:20  2020-03-31 14:49:20  2020-04-30 14:49:20
+										 2019-12-31 14:49:20  2019-11-30 14:49:20  2019-10-31 14:49:20
+		quarter 2020-01-31 14:49:20      2020-04-30 14:49:20  2020-07-31 14:49:20  2020-10-31 14:49:20
+										 2019-10-31 14:49:20  2019-07-31 14:49:20  2019-04-30 14:49:20
+		half year 2020-03-31 14:49:20    2020-09-30 14:49:20  2021-03-31 14:49:20  2021-09-30 14:49:20
+										 2019-09-30 14:49:20  2019-03-31 14:49:20  2018-09-30 14:49:20
+		year 2020-02-29 14:49:20         2021-02-28 14:49:20  2022-02-28 14:49:20  2023-02-28 14:49:20
+										 2019-02-28 14:49:20  2018-02-28 14:49:20  2017-02-28 14:49:20`
+	if d := ztest.Diff(have, want, ztest.DiffNormalizeWhitespace); d != "" {
+		t.Error(d)
 	}
 }
