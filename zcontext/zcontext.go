bad84e8c45707212cd464eca6d66a359223d9af2
Martin Tournoij <martin@arp242.net>
2021-10-10 13:03:26 +0800

    Add zcontext.WithoutTimeout()

---
+++ zcontext/zcontext.go
@@ -0,0 +1,17 @@
+package zcontext
+
+import (
+	"context"
+	"time"
+)
+
+type ntContext struct{ p context.Context }
+
+func (ntContext) Deadline() (time.Time, bool)         { return time.Time{}, false }
+func (ntContext) Done() <-chan struct{}               { return nil }
+func (ntContext) Err() error                          { return nil }
+func (c ntContext) Value(key interface{}) interface{} { return c.p.Value(key) }
+
+// WithoutTimeout returns a new context without any cancellations from
+// WithTimeout() or WithDeadline(), but preserves any values.
+func WithoutTimeout(ctx context.Context) context.Context { return ntContext{ctx} }
---
+++ zcontext/zcontext_test.go
@@ -0,0 +1,35 @@
+package zcontext
+
+import (
+	"context"
+	"testing"
+	"time"
+)
+
+func TestNoCancel(t *testing.T) {
+	ctx := context.WithValue(context.Background(), "a", "value a")
+	ctx, c := context.WithTimeout(ctx, 50*time.Millisecond)
+	defer c()
+	ctx = context.WithValue(ctx, "b", "value b")
+
+	ctx2 := WithoutTimeout(ctx)
+	if ctx2.Value("a").(string) != "value a" {
+		t.Fatal()
+	}
+	if ctx2.Value("b").(string) != "value b" {
+		t.Fatal()
+	}
+
+	c()
+	select {
+	case <-ctx2.Done():
+		t.Fatal()
+	case <-time.After(100 * time.Millisecond):
+	}
+	if ctx2.Err() != nil {
+		t.Fatal()
+	}
+	if ctx.Err() == nil {
+		t.Fatal()
+	}
+}
