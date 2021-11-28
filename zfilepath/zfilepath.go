6c7f831363c2ef0ae9e1d4ef4636a9b8c5e10f67
Martin Tournoij <martin@arp242.net>
2021-11-28 02:05:47 +0100

    Add zfilepath.TrimPrefix

--- zfilepath/zfilepath.go
+++ zfilepath/zfilepath.go
@@ -1,7 +1,10 @@
 // Package zfilepath implements functions for manipulating filename paths.
 package zfilepath
 
-import "path/filepath"
+import (
+	"path/filepath"
+	"strings"
+)
 
 // SplitExt returns the path without extension and the extension.
 //
@@ -13,4 +16,15 @@ func SplitExt(path string) (string, string) {
 		return path, ""
 	}
 	return path[:len(path)-len(e)], e[1:]
+}
+
+// TrimPrefix trims a prefix from a path.
+//
+// This ensures that any trailing slashes are also removed.
+func TrimPrefix(path, prefix string) string {
+	path = strings.TrimPrefix(path, prefix)
+	if len(path) > 0 && (path[0] == '/' || path[0] == '\\') {
+		return path[1:]
+	}
+	return path
 }
--- zfilepath/zfilepath_test.go
+++ zfilepath/zfilepath_test.go
@@ -2,6 +2,27 @@ package zfilepath
 
 import "testing"
 
+func TestTrimPrefix(t *testing.T) {
+	tests := []struct {
+		path, prefix, want string
+	}{
+		{"", "", ""},
+		{"/", "/", ""},
+		{"/etc/passwd", "/etc", "passwd"},
+		{"/etc/passwd", "/etc/", "passwd"},
+	}
+
+	for _, tt := range tests {
+		t.Run("", func(t *testing.T) {
+			have := TrimPrefix(tt.path, tt.prefix)
+
+			if have != tt.want {
+				t.Errorf("\nhave: %q\nwant: %q", have, tt.want)
+			}
+		})
+	}
+}
+
 func TestSplitExt(t *testing.T) {
 	tests := []struct {
 		in, path, ext string
@@ -17,10 +38,10 @@ func TestSplitExt(t *testing.T) {
 			p, e := SplitExt(tt.in)
 
 			if p != tt.path {
-				t.Errorf("\ngot:  %q\nwant: %q", p, tt.path)
+				t.Errorf("\nhave: %q\nwant: %q", p, tt.path)
 			}
 			if e != tt.ext {
-				t.Errorf("\ngot:  %q\nwant: %q", e, tt.ext)
+				t.Errorf("\nhave: %q\nwant: %q", e, tt.ext)
 			}
 		})
 	}
