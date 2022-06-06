b3b912008eb0c60cb80a2c8863b578a1704c3d9b
Martin Tournoij <martin@arp242.net>
2022-06-06 11:03:08 +0200

    Add zreflect.Tag

--- zreflect/zreflect.go
+++ zreflect/zreflect.go
@@ -1,1 +1,21 @@
 package zreflect
+
+import (
+	"reflect"
+	"strings"
+)
+
+// Tag splits the tag in to the tag name and options.
+func Tag(field reflect.StructField, tag string) (string, []string) {
+	t, ok := field.Tag.Lookup(tag)
+	if !ok {
+		return "", nil
+	}
+
+	sp := strings.Split(t, ",")
+	if len(sp) > 1 {
+		return sp[0], sp[1:]
+	}
+
+	return t, nil
+}
--- zreflect/zreflect_test.go
+++ zreflect/zreflect_test.go
@@ -1,1 +1,58 @@
 package zreflect
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestTag(t *testing.T) {
+	tests := []struct {
+		in      reflect.StructField
+		tag     string
+		wantTag string
+		wantOpt []string
+	}{
+		{
+			func() reflect.StructField {
+				return reflect.TypeOf(struct {
+					XXX string `json:"xxx" db:"yyy"`
+				}{}).Field(0)
+			}(),
+			"json",
+			"xxx", nil,
+		},
+
+		{
+			func() reflect.StructField {
+				return reflect.TypeOf(struct {
+					XXX string `json:"xxx,opt1,opt2" db:"yyy"`
+				}{}).Field(0)
+			}(),
+			"json",
+			"xxx", []string{"opt1", "opt2"},
+		},
+
+		{
+			func() reflect.StructField {
+				return reflect.TypeOf(struct {
+					XXX string `db:"yyy"`
+				}{}).Field(0)
+			}(),
+			"json",
+			"", nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run("", func(t *testing.T) {
+			tag, opt := Tag(tt.in, tt.tag)
+
+			if tag != tt.wantTag {
+				t.Errorf("\nhave: %q\nwant: %q", tag, tt.wantTag)
+			}
+			if !reflect.DeepEqual(opt, tt.wantOpt) {
+				t.Errorf("\nhave: %#v\nwant: %#v", opt, tt.wantOpt)
+			}
+		})
+	}
+}
