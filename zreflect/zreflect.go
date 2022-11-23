f4a7c064118033e44d5b84ceec1ec4072488c1a9
Martin Tournoij <martin@arp242.net>
2022-11-23 12:38:14 +0100

    Add zreflect.Fields()

--- zreflect/zreflect.go
+++ zreflect/zreflect.go
@@ -4,6 +4,8 @@ package zreflect
 import (
 	"reflect"
 	"strings"
+
+	"zgo.at/zstd/internal/exp/slices"
 )
 
 // Tag splits the tag in to the tag name and options.
@@ -19,4 +21,93 @@ func Tag(field reflect.StructField, tag string) (string, []string) {
 	}
 
 	return t, nil
+}
+
+// Fields gets all fields for the struct t, as a slice of names and values, in
+// the order they are in the struct.
+//
+// If tag is not an empty string it will return the value of the tag as the
+// field name, or the struct field name if it's not set. Tags with a value of
+// "-" will be skipped.
+//
+// Fields will be skipped if the option given in noskip is set.
+//
+// It will panic if t is not a struct.
+//
+// For example:
+//
+//	t := struct {
+//		One   string `db:"one"`
+//		Two   string `db:"two,noinsert"`
+//		Three int
+//	}{"xxx", "yyy", 42}
+//
+//	Fields(t, "db", "noinsert")
+//
+// Will return:
+//
+//	[]string{"one", "Three"}
+//	[]any{"xxx", 42}
+func Fields(t any, tagname, skip string) (names []string, vals []any) {
+	var (
+		values = reflect.ValueOf(t)
+		types  = reflect.TypeOf(t)
+	)
+	for values.Kind() == reflect.Ptr {
+		values = values.Elem()
+		types = types.Elem()
+	}
+
+	if tagname == "" && skip != "" {
+		panic("zreflect.Fields: setting skip without tagname doesn't make much sense")
+	}
+	if values.Kind() != reflect.Struct {
+		panic("zreflect.Fields: not a struct")
+	}
+
+	n := values.NumField()
+	names = make([]string, 0, n)
+	vals = make([]any, 0, n)
+	for i := 0; n > i; i++ {
+		t := types.Field(i)
+		name := t.Name
+		if tagname != "" {
+			tag := t.Tag.Get(tagname)
+			if tag == "-" {
+				continue
+			}
+			tname, opt, _ := strings.Cut(tag, ",")
+			if opt != "" && slices.Contains(strings.Split(opt, ","), skip) {
+				continue
+			}
+			if tname != "" {
+				name = tname
+			}
+		}
+
+		names = append(names, name)
+		vals = append(vals, values.Field(i).Interface())
+	}
+	return names, vals
+}
+
+// Note these can be made a bit faster by re-implementing them, but without
+// adding the information we don't need:
+//
+//    BenchmarkFields-2        2369833              5082 ns/op            1496 B/op         33 allocs/op
+//    BenchmarkNames-2         2690127              4440 ns/op            1024 B/op         31 allocs/op
+//    BenchmarkValues-2        2330048              5151 ns/op            1472 B/op         32 allocs/op
+//
+// But for now, not really worth the effort.
+
+// Names is like [Fields], but only returns the names.
+func Names(t any, tagname, skip string) []string {
+	names, _ := Fields(t, tagname, skip)
+	return names
+}
+
+// Values is like [Fields], but only returns the values.
+func Values(t any, tagname, skip string) []any {
+	_, vals := Fields(t, tagname, skip)
+	return vals
 }
--- zreflect/zreflect_test.go
+++ zreflect/zreflect_test.go
@@ -3,6 +3,7 @@ package zreflect
 import (
 	"reflect"
 	"testing"
+	"time"
 )
 
 func TestTag(t *testing.T) {
@@ -55,4 +56,116 @@ func TestTag(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestFields(t *testing.T) {
+	i := 6
+	tests := []struct {
+		in         any
+		wantNames  []string
+		wantValues []any
+	}{
+		{struct{}{}, []string{}, []any{}},
+
+		{struct {
+			F1 int `db:"f1"`
+			F2 int `db:"f2,skip"`
+			F3 int
+			F4 int `db:",skip"`
+			F5 int `db:"-"`
+		}{1, 2, 3, 4, 5}, []string{"f1", "F3"}, []any{1, 3}},
+
+		{&struct {
+			F1 int `db:"f1"`
+			F2 int `db:"f2,skip"`
+			F3 int
+			F4 int `db:",skip"`
+			F5 int `db:"-"`
+			F6 *int
+		}{1, 2, 3, 4, 5, &i}, []string{"f1", "F3", "F6"}, []any{1, 3, &i}},
+
+		{struct {
+			N struct{ I int }
+		}{struct{ I int }{42}}, []string{"N"}, []any{struct{ I int }{42}}},
+	}
+
+	for _, tt := range tests {
+		t.Run("", func(t *testing.T) {
+			haveNames, haveValues := Fields(tt.in, "db", "skip")
+			if !reflect.DeepEqual(haveNames, tt.wantNames) {
+				t.Errorf("\nhave: %#v\nwant: %#v", haveNames, tt.wantNames)
+			}
+			if !reflect.DeepEqual(haveValues, tt.wantValues) {
+				t.Errorf("\nhave: %#v\nwant: %#v", haveValues, tt.wantValues)
+			}
+		})
+	}
+}
+
+var g1, g2 any
+
+type Strukt struct {
+	ID                   string    `db:"log_postgres_id,noinsert"`
+	LogTime              time.Time `db:"log_time"`
+	UserName             *string   `db:"user_name"`
+	DatabaseName         *string   `db:"database_name"`
+	ProcessID            int       `db:"process_id"`
+	ConnectionFrom       *string   `db:"connection_from"`
+	SessionID            string    `db:"session_id"`
+	SessionLineNum       int64     `db:"session_line_num"`
+	CommandTag           *string   `db:"command_tag"`
+	SessionStartTime     time.Time `db:"session_start_time"`
+	VirtualTransactionID *string   `db:"virtual_transaction_id"`
+	TransactionID        int64     `db:"transaction_id"`
+	ErrorSeverity        string    `db:"error_severity"`
+	SQLStateCode         string    `db:"sql_state_code"`
+	Message              string    `db:"message"`
+	Detail               *string   `db:"detail"`
+	Hint                 *string   `db:"hint"`
+	InternalQuery        *string   `db:"internal_query"`
+	InternalQueryPos     *int      `db:"internal_query_pos"`
+	Context              *string   `db:"context"`
+	Query                *string   `db:"query"`
+	QueryPos             *int      `db:"query_pos"`
+	Location             *string   `db:"location"`
+	ApplicationName      string    `db:"application_name"`
+	BackendType          string    `db:"backend_type"`
+	LeaderPID            *int      `db:"leader_pid"`
+	QueryID              int64     `db:"query_id"`
+}
+
+func BenchmarkTag(b *testing.B) {
+	f := reflect.TypeOf(Strukt{}).Field(0)
+	var v1, v2 any
+	for n := 0; n < b.N; n++ {
+		v1, v2 = Tag(f, "db")
+	}
+	g1, g2 = v1, v2
+}
+
+func BenchmarkFields(b *testing.B) {
+	var s Strukt
+	var v1, v2 any
+	for n := 0; n < b.N; n++ {
+		v1, v2 = Fields(s, "db", "noinsert")
+	}
+	g1, g2 = v1, v2
+}
+
+func BenchmarkNames(b *testing.B) {
+	var s Strukt
+	var v1 any
+	for n := 0; n < b.N; n++ {
+		v1 = Names(s, "db", "noinsert")
+	}
+	g1 = v1
+}
+
+func BenchmarkValues(b *testing.B) {
+	var s Strukt
+	var v1 any
+	for n := 0; n < b.N; n++ {
+		v1 = Values(s, "db", "noinsert")
+	}
+	g1 = v1
 }
