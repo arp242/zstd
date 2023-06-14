cfe1347660e33ff6a83e7c61d45811d8f95d2f80
Martin Tournoij <martin@arp242.net>
2023-06-14 16:40:57 +0200

    Also return tag options from zreflect.Fields()

--- zreflect/zreflect.go
+++ zreflect/zreflect.go
@@ -23,14 +23,13 @@ func Tag(field reflect.StructField, tag string) (string, []string) {
 	return t, nil
 }
 
-// Fields gets all fields for the struct t, as a slice of names and values, in
-// the order they are in the struct.
+// Fields gets all exported fields for the struct t, as a slice of names,
+// values, and tag options, in the order they are in the struct.
 //
-// If tag is not an empty string it will return the value of the tag as the
-// field name, or the struct field name if it's not set. Tags with a value of
-// "-" will be skipped.
+// If tag is not an "" it will use the tag name as the field name, falling back
+// to the field name if it's not set. Tags with a value of "-" will be skipped.
 //
-// Fields will be skipped if the option given in noskip is set.
+// Fields will be skipped if the option given in skip is set in the tag.
 //
 // It will panic if t is not a struct.
 //
@@ -48,7 +47,8 @@ func Tag(field reflect.StructField, tag string) (string, []string) {
 //
 //	[]string{"one", "Three"}
 //	[]any{"xxx", 42}
-func Fields(t any, tagname, skip string) (names []string, vals []any) {
+//	[][]string{nil, []string{"noinsert"}}
+func Fields(t any, tagname, skip string) (names []string, vals []any, opts [][]string) {
 	var (
 		values = reflect.ValueOf(t)
 		types  = reflect.TypeOf(t)
@@ -68,54 +68,56 @@ func Fields(t any, tagname, skip string) (names []string, vals []any) {
 	n := values.NumField()
 	names = make([]string, 0, n)
 	vals = make([]any, 0, n)
+	opts = make([][]string, 0, n)
 	for i := 0; n > i; i++ {
 		t := types.Field(i)
+		if !t.IsExported() {
+			continue
+		}
 
-		if t.Type.Kind() == reflect.Struct && t.Anonymous { // Embedded struct
-			en, ev := Fields(values.Field(i).Interface(), tagname, skip)
-			names = append(names, en...)
-			vals = append(vals, ev...)
+		if t.Type.Kind() == reflect.Struct && t.Anonymous { /// Embedded struct
+			en, ev, op := Fields(values.Field(i).Interface(), tagname, skip)
+			names, vals, opts = append(names, en...), append(vals, ev...), append(opts, op...)
 			continue
 		}
 
 		name := t.Name
+		var opt []string
 		if tagname != "" {
-			tag := t.Tag.Get(tagname)
-			if tag == "-" {
-				continue
-			}
-			tname, opt, _ := strings.Cut(tag, ",")
-			if opt != "" && slices.Contains(strings.Split(opt, ","), skip) {
+			tname, o := Tag(t, tagname)
+			if tname == "-" || slices.Contains(o, skip) {
 				continue
 			}
 			if tname != "" {
 				name = tname
 			}
+			opt = o
 		}
 
 		names = append(names, name)
 		vals = append(vals, values.Field(i).Interface())
+		opts = append(opts, opt)
 	}
-	return names, vals
+	return names, vals, opts
 }
 
-// Note these can be made a bit faster by re-implementing them, but without
-// adding the information we don't need:
+// These can be made a bit faster by re-implementing them instead of using
+// Fields() without adding the information we don't need:
 //
 //    BenchmarkFields-2        2369833              5082 ns/op            1496 B/op         33 allocs/op
 //    BenchmarkNames-2         2690127              4440 ns/op            1024 B/op         31 allocs/op
 //    BenchmarkValues-2        2330048              5151 ns/op            1472 B/op         32 allocs/op
 //
-// But for now, not really worth the effort.
+// But the difference seems small enough that it doesn't really matter.
 
 // Names is like [Fields], but only returns the names.
 func Names(t any, tagname, skip string) []string {
-	names, _ := Fields(t, tagname, skip)
+	names, _, _ := Fields(t, tagname, skip)
 	return names
 }
 
 // Values is like [Fields], but only returns the values.
 func Values(t any, tagname, skip string) []any {
-	_, vals := Fields(t, tagname, skip)
+	_, vals, _ := Fields(t, tagname, skip)
 	return vals
 }
--- zreflect/zreflect_test.go
+++ zreflect/zreflect_test.go
@@ -2,8 +2,11 @@ package zreflect
 
 import (
 	"reflect"
+	"strings"
 	"testing"
 	"time"
+
+	"zgo.at/zstd/ztype"
 )
 
 func TestTag(t *testing.T) {
@@ -58,18 +61,35 @@ func TestTag(t *testing.T) {
 	}
 }
 
-func TestFields(t *testing.T) {
-	type EmbedMe struct {
+type (
+	EmbedMe struct {
 		FE string `db:"fe"`
 	}
+	EmbedUnexport struct {
+		FE    string `db:"fe"`
+		unexp int
+	}
+	embedUnexport struct {
+		FE    string `db:"fe"`
+		unexp int
+	}
+)
 
-	i := 6
+func TestFields(t *testing.T) {
 	tests := []struct {
 		in         any
 		wantNames  []string
 		wantValues []any
+		wantOpts   [][]string
 	}{
-		{struct{}{}, []string{}, []any{}},
+		{struct{}{}, []string{}, []any{}, [][]string{}},
+
+		{ztype.Ptr(struct {
+			F1 int `db:"f1,opt"`
+		}{42}),
+			[]string{"f1"},
+			[]any{42},
+			[][]string{{"opt"}}},
 
 		{struct {
 			F1 int `db:"f1"`
@@ -77,7 +97,12 @@ func TestFields(t *testing.T) {
 			F3 int
 			F4 int `db:",skip"`
 			F5 int `db:"-"`
-		}{1, 2, 3, 4, 5}, []string{"f1", "F3"}, []any{1, 3}},
+			F6 int `db:"f6,opt"`
+			F7 int `db:"f7,opt,other"`
+		}{1, 2, 3, 4, 5, 6, 7},
+			[]string{"f1", "F3", "f6", "f7"},
+			[]any{1, 3, 6, 7},
+			[][]string{nil, nil, []string{"opt"}, []string{"opt", "other"}}},
 
 		{&struct {
 			F1 int `db:"f1"`
@@ -86,37 +111,110 @@ func TestFields(t *testing.T) {
 			F4 int `db:",skip"`
 			F5 int `db:"-"`
 			F6 *int
-		}{1, 2, 3, 4, 5, &i}, []string{"f1", "F3", "F6"}, []any{1, 3, &i}},
+		}{1, 2, 3, 4, 5, ztype.Ptr(6)},
+			[]string{"f1", "F3", "F6"},
+			[]any{1, 3, ztype.Ptr(6)},
+			[][]string{nil, nil, nil}},
 
 		{struct {
 			N struct{ I int }
-		}{struct{ I int }{42}}, []string{"N"}, []any{struct{ I int }{42}}},
+		}{struct{ I int }{42}},
+			[]string{"N"},
+			[]any{struct{ I int }{42}},
+			[][]string{nil}},
 
 		{struct {
 			EmbedMe
 			F1 int `db:"f1"`
-		}{EmbedMe{"XXX"}, 666}, []string{"fe", "f1"}, []any{"XXX", 666}},
+		}{EmbedMe{"XXX"}, 666},
+			[]string{"fe", "f1"},
+			[]any{"XXX", 666},
+			[][]string{nil, nil}},
 
 		{struct {
 			EmbedMe EmbedMe
 			F1      int `db:"f1"`
-		}{EmbedMe{"XXX"}, 666}, []string{"EmbedMe", "f1"}, []any{EmbedMe{"XXX"}, 666}},
+		}{EmbedMe{"XXX"}, 666},
+			[]string{"EmbedMe", "f1"},
+			[]any{EmbedMe{"XXX"}, 666},
+			[][]string{nil, nil}},
+
+		{struct {
+			unexport1 int
+			unexport2 int `db:"asd"`
+		}{1, 2}, []string{}, []any{}, [][]string{}},
+
+		{ztype.Ptr(struct {
+			embedUnexport
+			F1 int `db:"f1,opt"`
+		}{embedUnexport{"X", 1}, 42}),
+			[]string{"f1"},
+			[]any{42},
+			[][]string{{"opt"}}},
+
+		{ztype.Ptr(struct {
+			EmbedUnexport
+			F1 int `db:"f1,opt"`
+		}{EmbedUnexport{"X", 1}, 42}),
+			[]string{"fe", "f1"},
+			[]any{"X", 42},
+			[][]string{nil, {"opt"}}},
 	}
 
 	for _, tt := range tests {
 		t.Run("", func(t *testing.T) {
-			haveNames, haveValues := Fields(tt.in, "db", "skip")
+			haveNames, haveValues, haveOpts := Fields(tt.in, "db", "skip")
 			if !reflect.DeepEqual(haveNames, tt.wantNames) {
-				t.Errorf("\nhave: %#v\nwant: %#v", haveNames, tt.wantNames)
+				t.Errorf("wrong names\nhave: %#v\nwant: %#v", haveNames, tt.wantNames)
 			}
 			if !reflect.DeepEqual(haveValues, tt.wantValues) {
-				t.Errorf("\nhave: %#v\nwant: %#v", haveValues, tt.wantValues)
+				t.Errorf("wrong values\nhave: %#v\nwant: %#v", haveValues, tt.wantValues)
+			}
+			if !reflect.DeepEqual(haveOpts, tt.wantOpts) {
+				t.Errorf("wrong options\nhave: %#v\nwant: %#v", haveOpts, tt.wantOpts)
+			}
+
+			if n := Names(tt.in, "db", "skip"); !reflect.DeepEqual(n, haveNames) {
+				t.Errorf("Names() has different return\nhave:  %#v\nnames: %#v", haveNames, n)
+			}
+			if v := Values(tt.in, "db", "skip"); !reflect.DeepEqual(v, haveValues) {
+				t.Errorf("Values() has different return\nhave:   %#v\nvalues: %#v", haveValues, v)
 			}
 		})
 	}
 }
 
-var g1, g2 any
+func TestFieldsPanic(t *testing.T) {
+	t.Run("not a struct", func(t *testing.T) {
+		var rec any
+		func() {
+			defer func() { rec = recover() }()
+			Fields(123, "", "")
+		}()
+		if rec == nil {
+			t.Fatal("no panic")
+		}
+		if !strings.Contains(rec.(string), "not a struct") {
+			t.Fatalf("wrong panic: %s", rec)
+		}
+	})
+
+	t.Run("skip without tag", func(t *testing.T) {
+		var rec any
+		func() {
+			defer func() { rec = recover() }()
+			Fields(struct{}{}, "", "skip")
+		}()
+		if rec == nil {
+			t.Fatal("no panic")
+		}
+		if !strings.Contains(rec.(string), "skip without tagname") {
+			t.Fatalf("wrong panic: %s", rec)
+		}
+	})
+}
+
+var g1, g2, g3 any
 
 type Strukt struct {
 	ID                   string    `db:"log_postgres_id,noinsert"`
@@ -159,11 +257,11 @@ func BenchmarkTag(b *testing.B) {
 
 func BenchmarkFields(b *testing.B) {
 	var s Strukt
-	var v1, v2 any
+	var v1, v2, v3 any
 	for n := 0; n < b.N; n++ {
-		v1, v2 = Fields(s, "db", "noinsert")
+		v1, v2, v3 = Fields(s, "db", "noinsert")
 	}
-	g1, g2 = v1, v2
+	g1, g2, g3 = v1, v2, v3
 }
 
 func BenchmarkNames(b *testing.B) {
