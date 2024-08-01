977f077a1a7fe8165830378f7384434b986f0c50
Martin Tournoij <martin@arp242.net>
2024-08-01 19:01:55 +0100

    Add zmap.Values()

--- zmap/zmap.go
+++ zmap/zmap.go
@@ -36,3 +36,12 @@ func LongestKey[M ~map[string]V, V any](m M) ([]string, int) {
 	}
 	return r, l
 }
+
+// Values returns the values of the map.
+func Values[M ~map[K]V, K comparable, V any](m M) []V {
+	r := make([]V, 0, len(m))
+	for _, v := range m {
+		r = append(r, v)
+	}
+	return r
+}
