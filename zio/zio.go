6cca4111c60805449ec1c82fc4b6fffcc26bb2f2
Martin Tournoij <martin@arp242.net>
2024-09-30 16:31:30 +0100

    Add zio.LimitReader

--- zio/zio.go
+++ zio/zio.go
@@ -190,3 +190,38 @@ func (r *peekReader) Close() error {
 	}
 	return nil
 }
+
+// LimitReader returns a Reader that reads from r but stops with EOF after n
+// bytes. The underlying implementation is a *LimitedReader.
+//
+// This is identical to [io.LimitReader], except that it accepts a ReadCloser
+// (and supports Close).
+func LimitReader(r io.ReadCloser, n int64) io.ReadCloser { return &LimitedReader{r, n} }
+
+// A LimitedReader reads from R but limits the amount of data returned to just N
+// bytes. Each call to Read updates N to reflect the new amount remaining.
+//
+// Read returns EOF when N <= 0 or when the underlying R returns EOF.
+//
+// This is identical to [io.LimitedReader], except that it accepts a ReadCloser
+// (and supports Close).
+type LimitedReader struct {
+	R io.ReadCloser // underlying reader
+	N int64         // max bytes remaining
+}
+
+func (l *LimitedReader) Read(p []byte) (n int, err error) {
+	if l.N <= 0 {
+		return 0, io.EOF
+	}
+	if int64(len(p)) > l.N {
+		p = p[0:l.N]
+	}
+	n, err = l.R.Read(p)
+	l.N -= int64(n)
+	return
+}
+
+func (l *LimitedReader) Close() error {
+	return l.R.Close()
+}
