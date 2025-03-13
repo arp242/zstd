0355508dd006b404926945f59bead0ae2cd5d3dd
Martin Tournoij <martin@arp242.net>
2025-03-13 02:13:12 +0000

    Fix test

--- ztest/diff_test.go
+++ ztest/diff_test.go
@@ -147,7 +147,9 @@ func TestDiffMatch(t *testing.T) {
 		{"Hello", "He%(ANY)", ""},
 
 		{"Hello " + year + "!", "Hello %(YEAR)!", ""},
-		{"Hello " + year + "!", "Hello %(YEAR)", "\n--- have\n+++ want\n@@ -1 +1 @@\n-have Hello 2024!\n+want Hello 2024\n"},
+		{"Hello " + year + "!", "Hello %(YEAR)",
+			fmt.Sprintf("\n--- have\n+++ want\n@@ -1 +1 @@\n-have Hello %[1]d!\n+want Hello %[1]d\n",
+				time.Now().Year())},
 
 		{"Hello xy", "Hello %(ANY 2)", ""},
 		{"Hello xy", "Hello %(ANY 2,)", ""},
