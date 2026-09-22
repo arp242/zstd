arp242/zstd

commit 2f1a66cd092f28655910e91e482488ea2f835518

Use json/v2, remove most stuff from zjson package
Some are now redundant because we can pass a list of options, and some
others were never really used.

ztest/diff.go
@@ -31,7 +31,8 @@
 package ztest
 
 import (
-	"encoding/json"
+	"encoding/json/jsontext"
+	"encoding/json/v2"
 	"fmt"
 	"regexp"
 	"strings"
@@ -562,5 +563,5 @@ func indentJSON(data []byte, v any, prefix, indent string) ([]byte, error) {
 	if err != nil {
 		return nil, err
 	}
-	return json.MarshalIndent(v, prefix, indent)
+	return json.Marshal(v, jsontext.WithIndentPrefix(prefix), jsontext.WithIndent(indent))
 }

zint/bitflag_test.go
@@ -2,7 +2,7 @@ package zint
 
 import (
 	"encoding"
-	"encoding/json"
+	"encoding/json/v2"
 	"fmt"
 	"testing"
 )

zjson/zjson.go
@@ -2,201 +2,23 @@
 package zjson
 
 import (
-	"bytes"
-	"encoding/json"
-	"errors"
+	"encoding/json/v2"
 	"fmt"
-	"reflect"
-	"strconv"
-	"strings"
-	"time"
 )
 
-// Int for APIs that return numbers as strings.
-type Int int64
-
-// Marshal in to JSON.
-func (i Int) MarshalJSON() ([]byte, error) {
-	return append(append([]byte(`"`), strconv.FormatInt(int64(i), 10)...), '"'), nil
-}
-
-// Unmarshal a string timestamp as an int.
-func (i *Int) UnmarshalJSON(v []byte) error {
-	ii, err := strconv.ParseInt(string(bytes.Trim(v, `"`)), 10, 64)
-	*i = Int(ii)
-	return err
-}
-
-// Timestamp for APIs that return dates as a numeric Unix timestamp.
-type Timestamp struct{ time.Time }
-
-// Marshal in to JSON.
-func (t Timestamp) MarshalJSON() ([]byte, error) {
-	return []byte(strconv.FormatInt(t.Unix(), 10)), nil
-}
-
-// Unmarshal a Unix timestamp as a date.
-func (t *Timestamp) UnmarshalJSON(v []byte) error {
-	t.Time = time.Time{}
-
-	vv := string(v)
-	if vv == "null" || vv == "undefined" || vv == "" || vv == "0" {
-		return nil
-	}
-
-	n, err := strconv.ParseInt(vv, 10, 64)
-	if err != nil {
-		return fmt.Errorf("Timestamp.UnmarshalJSON %q: %w", vv, err)
-	}
-	if n > 0 { // Make sure that IsZero() works.
-		t.Time = time.Unix(n, 0).UTC()
-	}
-	return nil
-}
-
-// UnmarshalTo unmarshals the JSON in data to a new instance of target.
-//
-// Target can be any type that json.Unmarshal can unmarshal to, and doesn't need
-// to be a pointer. The returned value is always a pointer.
-func UnmarshalTo(data []byte, target reflect.Type) (any, error) {
-	if target == nil {
-		return nil, errors.New("zjson.UnmarshalTo: target is nil")
-	}
-	for target.Kind() == reflect.Pointer {
-		target = target.Elem()
-	}
-
-	t := reflect.New(target).Interface()
-	return t, json.Unmarshal(data, t)
-}
-
-// MustUnmarshalTo behaves like UnmarshalTo but will panic on errors.
-func MustUnmarshalTo(data []byte, target reflect.Type) any {
-	t, err := UnmarshalTo(data, target)
-	if err != nil {
-		panic(err)
-	}
-	return t
-}
-
 // MustMarshal behaves like json.Marshal but will panic on errors.
-func MustMarshal(v any) []byte {
-	b, err := json.Marshal(v)
+func MustMarshal(in any, opts ...json.Options) []byte {
+	b, err := json.Marshal(in, opts...)
 	if err != nil {
-		panic(err)
-	}
-	return b
-}
-
-// MustMarshalIndent behaves like json.MarshalIndent but will panic on errors.
-func MustMarshalIndent(v any, prefix, indent string) []byte {
-	b, err := json.MarshalIndent(v, prefix, indent)
-	if err != nil {
-		panic(err)
+		panic(fmt.Errorf("zjson.MustMarshal: %w", err))
 	}
 	return b
 }
 
 // MustUnmarshal behaves like json.Unmarshal but will panic on errors.
-func MustUnmarshal(data []byte, v any) {
-	err := json.Unmarshal(data, v)
+func MustUnmarshal(in []byte, out any, opts ...json.Options) {
+	err := json.Unmarshal(in, out, opts...)
 	if err != nil {
-		panic(err)
+		panic(fmt.Errorf("zjson.MustUnmarshal: %w", err))
 	}
-}
-
-// Indent a json string by unmarshalling it and marshalling it with
-// MarshalIndent.
-//
-// The data will be unmarshalled in to v, which must be a pointer. Example:
-//
-//	Indent(`{"a": "b"}`, &map[string]string{}, "", "  ")
-func Indent(data []byte, v any, prefix, indent string) ([]byte, error) {
-	err := json.Unmarshal(data, v)
-	if err != nil {
-		return nil, err
-	}
-	return json.MarshalIndent(v, prefix, indent)
-}
-
-// MustIndent behaves like Indent but will panic on errors.
-func MustIndent(data []byte, v any, prefix, indent string) []byte {
-	b, err := Indent(data, v, prefix, indent)
-	if err != nil {
-		panic(err)
-	}
-	return b
-}
-
-// MustMarshalString is like MustMarshal, but returns a string.
-func MustMarshalString(v any) string {
-	return string(MustMarshal(v))
-}
-
-// MustMarshalIndentString is like MustMarshalIndent, but returns a string.
-func MustMarshalIndentString(v any, prefix, indent string) string {
-	return string(MustMarshalIndent(v, prefix, indent))
-}
-
-// IndentString is like Indent, but returns a string.
-func IndentString(data []byte, v any, prefix, indent string) (string, error) {
-	b, err := Indent(data, v, prefix, indent)
-	return string(b), err
-}
-
-// MustIndentString is like MustIndent, but returns a string.
-func MustIndentString(data []byte, v any, prefix, indent string) string {
-	return string(MustIndent(data, v, prefix, indent))
-}
-
-// Types that can occur in JSON documents.
-type Types interface {
-	~float64 | ~string | ~[]any | ~map[string]any
-}
-
-// Get a value from JSON bytes.
-//
-// Format of path:
-//
-//	foo
-//	foo.nested
-func Get[T Types](j []byte, path string) (T, error) {
-	var (
-		zero  T
-		m     map[string]any
-		paths = strings.Split(path, ".")
-	)
-
-	err := json.Unmarshal(j, &m)
-	if err != nil {
-		return zero, fmt.Errorf("zjson.Get: %w", err)
-	}
-
-	for i, p := range paths {
-		v, ok := m[p]
-		if !ok {
-			return zero, fmt.Errorf("zjson.Get: field %q not found", path)
-		}
-
-		if i == len(paths)-1 {
-			vv, ok := v.(T)
-			if !ok {
-				return zero, fmt.Errorf("zjson.Get: field %q is wrong type (have: %T, want: %T)", p, v, zero)
-			}
-			return vv, nil
-		}
-
-		// TODO: also support arrays as "path.[0]", "path.[0].nest".
-		m = v.(map[string]any)
-	}
-	panic("unreachable")
-}
-
-// MustGet is like [Get], but will panic on errors.
-func MustGet[T Types](j []byte, path string) T {
-	v, err := Get[T](j, path)
-	if err != nil {
-		panic(err)
-	}
-	return v
 }

zjson/zjson_test.go
@@ -1,102 +1,12 @@
 package zjson
 
 import (
-	"encoding/json"
 	"fmt"
-	"net/mail"
 	"reflect"
 	"strings"
 	"testing"
 	"time"
 )
-
-func TestInt(t *testing.T) {
-	var x struct {
-		Int Int `json:"int"`
-	}
-
-	{ // Parse stings.
-		MustUnmarshal([]byte(`{"int":"1234567890"}`), &x)
-		have := fmt.Sprintf("%v", x)
-		want := "{1234567890}"
-		if have != want {
-			t.Errorf("Unmarshal\nhave: %q\nwant: %q", have, want)
-		}
-	}
-	{
-		have := string(MustMarshal(x))
-		want := `{"int":"1234567890"}`
-		if have != want {
-			t.Errorf("Marshal\nhave: %q\nwant: %q", have, want)
-		}
-	}
-
-	{ // Real ints work as well.
-		MustUnmarshal([]byte(`{"int":42}`), &x)
-		have := fmt.Sprintf("%v", x)
-		want := "{42}"
-		if have != want {
-			t.Errorf("Unmarshal\nhave: %q\nwant: %q", have, want)
-		}
-	}
-	{
-		have := string(MustMarshal(x))
-		want := `{"int":"42"}`
-		if have != want {
-			t.Errorf("Marshal\nhave: %q\nwant: %q", have, want)
-		}
-	}
-
-	{ // Non-numbers don't
-		err := json.Unmarshal([]byte(`{"int": "NaN"}`), &x)
-		if err == nil {
-			t.Errorf("no error on NaN")
-		}
-	}
-
-	{ // Neither do floats.
-		err := json.Unmarshal([]byte(`{"int": "42.666"}`), &x)
-		if err == nil {
-			t.Errorf("no error on NaN")
-		}
-	}
-}
-
-func TestTimestamp(t *testing.T) {
-	var x struct {
-		TS Timestamp `json:"ts"`
-	}
-
-	MustUnmarshal([]byte(`{"ts": 1234567890}`), &x)
-
-	out := fmt.Sprintf("%v", x)
-	want := "{2009-02-13 23:31:30 +0000 UTC}"
-	if out != want {
-		t.Errorf("Unmarshal\nout:  %q\nwant: %q", out, want)
-	}
-
-	out2 := string(MustMarshal(x))
-	want2 := `{"ts":1234567890}`
-	if out2 != want2 {
-		t.Errorf("Marshal\nout:  %q\nwant: %q", out2, want2)
-	}
-
-	err := json.Unmarshal([]byte(`{"ts": "NaN"}`), &x)
-	if err == nil {
-		t.Errorf("no error on NaN")
-	}
-
-	var zero struct {
-		TS Timestamp `json:"ts"`
-	}
-	err = json.Unmarshal([]byte(`{"ts": 0}`), &zero)
-	if err != nil {
-		t.Error(err)
-	}
-	if !zero.TS.IsZero() {
-		t.Errorf("not zero: %#v", x)
-	}
-}
 
 func TestMustMarshal(t *testing.T) {
 	cases := []struct {
@@ -109,42 +19,6 @@ func TestMustMarshal(t *testing.T) {
 	for i, tc := range cases {
 		t.Run(fmt.Sprintf("%v", i), func(t *testing.T) {
 			out := MustMarshal(tc.in)
-			if !reflect.DeepEqual(out, tc.want) {
-				t.Errorf("\nout:  %s\nwant: %s\n", out, tc.want)
-			}
-		})
-	}
-}
-
-func TestMustMarshalIndent(t *testing.T) {
-	cases := []struct {
-		in   map[string]string
-		want []byte
-	}{
-		{map[string]string{"hello": "world", "a": "b"}, []byte("{\n  \"a\": \"b\",\n  \"hello\": \"world\"\n}")},
-	}
-
-	for i, tc := range cases {
-		t.Run(fmt.Sprintf("%v", i), func(t *testing.T) {
-			out := MustMarshalIndent(tc.in, "", "  ")
-			if !reflect.DeepEqual(out, tc.want) {
-				t.Errorf("\nout:  %s\nwant: %s\n", out, tc.want)
-			}
-		})
-	}
-}
-
-func TestMustFormat(t *testing.T) {
-	cases := []struct {
-		in   []byte
-		want []byte
-	}{
-		{[]byte(`{"hello": "world", "a": "b"}`), []byte("{\n  \"a\": \"b\",\n  \"hello\": \"world\"\n}")},
-	}
-
-	for i, tc := range cases {
-		t.Run(fmt.Sprintf("%v", i), func(t *testing.T) {
-			out := MustIndent(tc.in, &map[string]string{}, "", "  ")
 			if !reflect.DeepEqual(out, tc.want) {
 				t.Errorf("\nout:  %s\nwant: %s\n", out, tc.want)
 			}
@@ -179,36 +53,6 @@ func TestMustUnmarshal(t *testing.T) {
 
 }
 
-func TestUnmarshalto(t *testing.T) {
-	tests := []struct {
-		json    string
-		target  reflect.Type
-		want    any
-		wantErr string
-	}{
-		{"", nil, nil, "target is nil"},
-
-		{`{}`, reflect.TypeFor[*mail.Address](), &mail.Address{}, ""},
-		{`{"Name": "Martin"}`, reflect.TypeFor[*mail.Address](), &mail.Address{Name: "Martin"}, ""},
-
-		{`{}`, reflect.TypeFor[mail.Address](), &mail.Address{}, ""},
-		{`{"Name": "Martin"}`, reflect.TypeFor[mail.Address](), &mail.Address{Name: "Martin"}, ""},
-	}
-
-	for _, tt := range tests {
-		t.Run("", func(t *testing.T) {
-			have, err := UnmarshalTo([]byte(tt.json), tt.target)
-			if !ErrorContains(err, tt.wantErr) {
-				t.Fatalf("wrong error\nhave: %v\nwant: %v\n", err, tt.wantErr)
-			}
-
-			if !reflect.DeepEqual(have, tt.want) {
-				t.Errorf("\nhave: %#v\nwant: %#v", have, tt.want)
-			}
-		})
-	}
-}
-
 // Import cycle
 func ErrorContains(have error, want string) bool {
 	if have == nil {
@@ -218,23 +62,4 @@ func ErrorContains(have error, want string) bool {
 		return false
 	}
 	return strings.Contains(have.Error(), want)
-}
-
-func TestGet(t *testing.T) {
-	if h, err := Get[float64]([]byte(`{"a":1}`), "a"); h != 1 {
-		t.Fatalf("wrong: %v; err: %s", h, err)
-	}
-	if h, err := Get[string]([]byte(`{"a":"b"}`), "a"); h != "b" {
-		t.Fatalf("wrong: %v; err: %s", h, err)
-	}
-	if h, err := Get[[]any]([]byte(`{"a":["b", "c"]}`), "a"); !reflect.DeepEqual(h, []any{"b", "c"}) {
-		t.Fatalf("wrong: %v; err: %s", h, err)
-	}
-	if h, err := Get[map[string]any]([]byte(`{"a":{"b":"c"}}`), "a"); !reflect.DeepEqual(h, map[string]any{"b": "c"}) {
-		t.Fatalf("wrong: %v; err: %s", h, err)
-	}
-
-	if h, err := Get[string]([]byte(`{"a":{"b":"c"}}`), "a.b"); h != "c" {
-		t.Fatalf("wrong: %v; err: %s", h, err)
-	}
 }

ztest/http.go
@@ -2,7 +2,7 @@ package ztest
 
 import (
 	"bytes"
-	"encoding/json"
+	"encoding/json/v2"
 	"io"
 	"mime/multipart"
 	"net/http"

ztype/optional.go
@@ -2,7 +2,7 @@ package ztype
 
 import (
 	"database/sql/driver"
-	"encoding/json"
+	"encoding/json/v2"
 	"fmt"
 )
 

commit b67327a0abc86921e5139525e7efb41ab9769c4d

Consistent json object order in ztest
Regression from 2f1a66c: json/v2 does not sort by default.

ztest/diff.go
@@ -563,5 +563,5 @@ func indentJSON(data []byte, v any, prefix, indent string) ([]byte, error) {
 	if err != nil {
 		return nil, err
 	}
-	return json.Marshal(v, jsontext.WithIndentPrefix(prefix), jsontext.WithIndent(indent))
+	return json.Marshal(v, jsontext.WithIndentPrefix(prefix), jsontext.WithIndent(indent), json.Deterministic(true))
 }

ztest/diff_test.go
@@ -255,7 +255,7 @@ func TestDiffJSON(t *testing.T) {
 
 		{`[1]`, `[1]`, ``},
 		{`"a"`, `"a"`, ``},
-		{`{"a": "x"}`, `{  "a":   "x"}`, ``},
+		{`{"a": "x", "b": "y"}`, `{  "a":   "x",   "b":        "y"}`, ``},
 
 		{`{"a": "x"}`, `{  "a":   "y"}`, `
 --- have